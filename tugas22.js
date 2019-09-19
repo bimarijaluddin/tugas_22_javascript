4b34593dbc523087dac7d7334f860f47dd3cc5ae
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 20:51:02 +0800

    Tugas 22 Javascript
    Tugas dari niomic

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function tugas22(){
+    var kata = ['Saya', 'ingin', 'belajar', 'bersama'];
+    kata.forEach(function(item,index){
+        console.log("Item : " + item + " Index ke " + index);
+    });
+}
+
+tugas22();
